ca3c25c918996d48b0ea9982de6b47e7d0e352b8
mohsin <mdmohsineng@gmail.com>
2023-08-31 13:42:15 +0600

    add home page

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
+        ],
+      ),
+      body: Center(
+        child: Text('HomePage'),
+      ),
+    );
+  }
+}
